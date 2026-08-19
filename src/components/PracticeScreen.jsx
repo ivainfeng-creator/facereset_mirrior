3ffa85d51b566fdf5438b4a6b700da0dd0d98635
Irma <Irma_Chen@htc.com>
2026-08-19 23:59:38 +0800

    feat: update PracticeScreen button with SVG icon and enhance styles for leaderboard card visibility

--- src/components/PracticeScreen.jsx
+++ src/components/PracticeScreen.jsx
@@ -81,7 +81,7 @@ export default function PracticeScreen({ selectedScene, stream, isDemoMode, skip
                 onBegin();
               }}
             >
-              I&apos;m ready <span aria-hidden="true">→</span>
+              I&apos;m ready <span aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" height="19.2px" viewBox="0 -960 960 960" width="19.2px" fill="#1f1f1f"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" /></svg></span>
             </button>
           </div>
         </section>
